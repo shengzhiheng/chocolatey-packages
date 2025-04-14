bbf090791c47b3774e9d5f47fcd41a7dbc3a694e
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2025-04-13 23:24:24 -0500

    Add quantum package

---
+++ quantum/README.md
@@ -0,0 +1,36 @@
+<img src="icon.ico" alt="drawing" width="128"/>
+
+# chocolatey-quantum
+
+Automatic qBittorrent port updater for ProtonVPN.
+
+## Installing
+
+### [chocolatey.org][1]
+
+[![](https://img.shields.io/chocolatey/v/audioling.svg)][1]
+
+```powershell
+choco install quantum
+```
+
+## Developing & Building
+
+```powershell
+> git clone https://github.com/shengzhiheng/chocolatey-packages.git
+> cd chocolatey-packages/quantum/
+quantum> Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Allow execution of powershell script
+quantum> .\build.ps1
+```
+
+This will create the following artifacts:
+
+- `.\quantum.{version}.nupkg`
+
+To push to chocolatey community package list, run:
+```powershell
+quantum> choco apikey add -k <your key here> -s https://push.chocolatey.org/
+quantum> choco push --source https://push.chocolatey.org/
+```
+
+[1]: https://chocolatey.org/packages/quantum
---
+++ quantum/build.ps1
@@ -0,0 +1,57 @@
+# build.ps1
+$PackageName = "Quantum"
+$Author = "UHAXM1"
+$ApiUrl = "https://api.github.com/repos/$Author/$PackageName/releases/latest"
+$Headers = @{"User-Agent" = "PowerShell"}
+try {
+    $Response = Invoke-RestMethod -Uri $ApiUrl -Headers $Headers
+    # The tag_name typically holds the release version/tag
+    $Version = $Response.tag_name -replace '^v',''
+} catch {
+    Write-Host "Failed to get latest release: $($_.Exception.Message)"
+}
+$InstallerUrl = "https://github.com/$Author/$PackageName/releases/download/v$Version/Quantum_Setup.exe"
+$TempFile = "$env:TEMP\QuantumInstaller.exe"
+
+# Step 1: Download installer
+Write-Host "Downloading $InstallerUrl..."
+Invoke-WebRequest -Uri $InstallerUrl -OutFile $TempFile
+
+# Step 2: Get checksum
+$Title = ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($TempFile).ProductName).Trim()
+$Copyright = ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($TempFile).LegalCopyright).Trim()
+$Checksum = (Get-FileHash $TempFile -Algorithm SHA256).Hash
+
+# Step 3: Generate chocolateyinstall.ps1 and uninstall.ps1 from template
+$templatePath = "./chocolateyinstall.ps1.tmpl"
+$installScript = "./chocolateyinstall.ps1"
+
+(Get-Content $templatePath) `
+  -replace "{{Url}}", $InstallerUrl `
+  -replace "{{Checksum}}", $Checksum `
+  | Set-Content $installScript
+
+$uninstallTemplate = "./chocolateyuninstall.ps1.tmpl"
+$uninstallScript = "./chocolateyuninstall.ps1"
+
+(Get-Content $uninstallTemplate) `
+  | Set-Content $uninstallScript
+
+# Step 4: Generate .nuspec from template
+$nuspecTemplate = "./quantum.nuspec.tmpl"
+$nuspecFile = "./quantum.nuspec"
+$nuspecContent = Get-Content $nuspecTemplate -Raw
+$nuspecContent = $nuspecContent `
+  -replace "{{PackageName}}", $PackageName `
+  -replace "{{Title}}", $Title `
+  -replace "{{Version}}", $Version `
+  -replace "{{Author}}", $Author `
+  -replace "{{Copyright}}", $Copyright `
+  | Set-Content $nuspecFile
+
+# Step 5: Build package
+Push-Location ./
+choco pack
+Pop-Location
+
+Write-Host "Package built: ./$PackageName.$Version.nupkg"
